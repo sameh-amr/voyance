60096c85f106f4720c59b63d4eed0f0844259c40
sameh-amr <samehamrsw@gmail.com>
2024-08-09 15:14:21 +0300

    added the initial configurations for the store

---
+++ src/redux/store/index.ts
@@ -0,0 +1,21 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "reduxjs-toolkit-persist";
+import storage from "reduxjs-toolkit-persist/lib/storage";
+import logger from "redux-logger";
+import {useDispatch,TypedUseSelectorHook,useSelector} from "react-redux"
+const appReducer = combineReducers({});
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, appReducer);
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+});
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof appReducer>;
+
+export const persistor = persistStore(store);
+export const useAppDispatch:()=>AppDispatch=useDispatch;
+export const useAppSelector:TypedUseSelectorHook<RootState>=useSelector;
