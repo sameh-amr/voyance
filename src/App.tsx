747d3b09f82353676b352695da0c857e8131dcb3
sameh-amr <samehamrsw@gmail.com>
2024-08-10 20:13:17 +0300

    added the protected route behaaviour for the system

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,39 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/login/login";
 import PatientDashboard from "./pages/patientdashboard/patientdashboard";
 import "react-datepicker/dist/react-datepicker.css";
 import "react-calendar/dist/Calendar.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import DoctorDashboard from "./pages/doctordashboard/doctordashboard";
+import { useSelector } from "react-redux";
+import { RootState } from "./redux/store";
+import ProtectedRoute from "./HOC/protectedroute/protectedroute";
+import { Roles } from "./constants/role";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const { isAuthenticated } = useSelector((state: RootState) => state.user);
+
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
-      <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
-      <Route path="/patient-dashboard" element={<PatientDashboard />} />
+      <Route
+        path="/doctor-dashboard"
+        element={
+          <ProtectedRoute role={Roles.DOCTOR}>
+            <DoctorDashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/patient-dashboard"
+        element={
+          <ProtectedRoute role={Roles.PATIENT}>
+            <PatientDashboard />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
--- src/HOC/protectedroute/protectedroute.tsx
+++ src/HOC/protectedroute/protectedroute.tsx
@@ -1,33 +1,57 @@
-// components/ProtectedRoute.tsx
-import React from "react";
-import { Route, Navigate, RouteProps } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 import { Roles } from "../../constants/role";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 interface ProtectedRouteProps {
-  element: React.ReactElement;
+  children: React.ReactElement;
   role?: Roles.DOCTOR | Roles.PATIENT;
 }
 
-const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
-  element,
-  role,
-  ...rest
-}) => {
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children, role }) => {
   const { isAuthenticated, role: userRole } = useSelector(
     (state: RootState) => state.user
   );
+  const [redirect, setRedirect] = useState(false);
 
-  if (!isAuthenticated) {
+  useEffect(() => {
+    if (!isAuthenticated || (role && role !== userRole)) {
+      toast.error("You are authorized to view this page. Redirecting to login", {
+        position: "top-right",
+        autoClose: 2000, // Auto-close after 2 seconds
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+
+      const timer = setTimeout(() => {
+        setRedirect(true);
+      }, 2000); // Delay redirection by 2 seconds to show the toast
+
+      return () => clearTimeout(timer); // Clean up the timer
+    }
+  }, [isAuthenticated, role, userRole]);
+
+  if (redirect) {
     return <Navigate to="/" />;
   }
 
-  if (role && role !== userRole) {
-    return <Navigate to="/" />;
+  if (isAuthenticated && (!role || role === userRole)) {
+    return (
+      <>
+        <ToastContainer />
+        {children}
+      </>
+    );
   }
 
-  return <Route {...rest} element={element} />;
+  return <ToastContainer />;
 };
 
 export default ProtectedRoute;
